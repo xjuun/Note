f501005d72baca937f7ebbe75ac01901c188cfb0
Ghostasky <2992721672@qq.com>
2021-08-14 19:27:23 +0800

    2021/8/14

--- Python/Base/5.py
+++ Python/Base/5.py
@@ -1,3 +1,18 @@
-import io
-from ecoji import encode
+import requests
+import hashlib
+from bs4 import BeautifulSoup
 
+req=requests.session()
+#change URL according to yours
+url="http://139.59.166.56:30481/"
+
+get_req=req.get(url)
+html=get_req.content
+
+soup = BeautifulSoup(html, 'html.parser')
+hash_val=soup.h3.get_text().encode('utf-8')
+
+md5hash=hashlib.md5(hash_val).hexdigest()
+data=dict(hash=md5hash)
+post_res=req.post(url=url,data=data)
+print("flag:",post_res.text)
